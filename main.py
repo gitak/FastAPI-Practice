79b990e0c7465cbfaaa2cc56cc229b87051eee99
gitak <gitak0127@gmail.com>
2024-04-25 11:14:15 +0900

    경로 매개변수 확인

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name" : model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name" : model_name, "message": "LeCNN all the images"}
+    return {"model_name" : model_name, "message": "Have some residuals"}
+
 
 @app.get("/")
 async def root():
@@ -14,7 +33,14 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int):
     return {"item_id": item_id}
 
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
 
