3fd2af7519e8713b66a5a1c807992567938677b9
gitak <gitak0127@gmail.com>
2024-04-25 15:43:16 +0900

    요청본문 연습

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
 from typing import Union
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price * item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+#     result = {"item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.dict()}
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#
+#     return item
 
 # fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 #
