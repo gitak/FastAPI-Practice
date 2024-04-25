43bb205e060624002507fbda8f89fe2cbb9fedfc
gitak <gitak0127@gmail.com>
2024-04-25 12:04:16 +0900

    쿼리 매개변수 연습

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
 from fastapi import FastAPI
+from typing import Union
 from enum import Enum
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name" : model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name" : model_name, "message": "LeCNN all the images"}
-    return {"model_name" : model_name, "message": "Have some residuals"}
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+#
+#
+# class ModelName(str, Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
+#
+#
+# # skip의 기본값은 0, limit의 기본값은 10
+#
+# @app.get("/items")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip: skip + limit]
+#
+# # 여러 경로/쿼리 매개변수(필수값이 아님)
+# @app.get("/items/{item_id}")
+# async def read_item_query(item_id: str, q: Union[str, None] = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+#
+# # None이 없다면 필수 매개변수
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+#
+# #필수 매개변수, 기본값이 있는 매개변수, 선택 매개변수
+# @app.get("/items/{item_id}")
+# async def read_user_item(
+#         item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+# ):
+#     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+#     return item
+#
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+#     return {"file_path": file_path}
+#
+#
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+#     return {"model_name": model_name, "message": "Have some residuals"}
+#
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+#
+#
+# # 경로 매개변수
+# # @app.get("/items/{item_id}")
+# # async def read_item(item_id: int):
+# #     return {"item_id": item_id}
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
+#
+#
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
