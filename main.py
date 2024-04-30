1d3fe4558f022d29970d39f5bbc070d7dc60ca82
gitak <gitak0127@gmail.com>
2024-04-30 17:17:08 +0900

    다중 매개변수 연습

--- main.py
+++ main.py
@@ -1,9 +1,60 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from typing import Union, List
 from enum import Enum
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+#
+# class User(BaseModel):
+#     username: str
+#     full_name: Union[str, None] = None
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(embed=True)):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#         *,
+#         item_id: int,
+#         item: Item,
+#         user: User,
+#         importance: int = Body(gt=0),
+#         q: Union[str, None] = None,
+#         ):
+#
+#         results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#         if q:
+#             results.update({"q" : q})
+#         return results
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+#     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#     return results
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#         *,
+#         item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+#         q: Union[str, None] = None,
+#         item: Union[Item, None] = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
 
 # @app.get("/items/{item_id}")
 # async def read_items(
